66e01ec5c09fc5bc67f6dca2a0a10453c88dd37c
Cabduwahaab <abdikarimabdi80@gmail.com>
2023-09-03 21:35:40 +0200

    initial commit

---
+++ public/js/mapbox.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+
+export const displayMap = locations => {
+  mapboxgl.accessToken = `pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A`;
+
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v11',
+    scrollZoom: false
+    //   interactive: false
+  });
+
+  const bounds = new mapboxgl.LngLatBounds();
+
+  locations.forEach(loc => {
+    // create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // add the marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
+
+    // add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description} </p>`)
+      .addTo(map);
+
+    // extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
+
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};
---
+++ public/js/stripe.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51NkBEPEKgKtpIhKBpAFESvySH8y1abeeuNbaMYkb8DVKu5O1IFnRZjH9reVRtc8PxiEDhMlAVjjdioUFWSPwvPbJ00OlShKSP6',
+  {
+    apiVersion: '2020-08-27' // Update the API version
+  }
+);
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios.get(
+      // Use axios.get instead of axios
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
